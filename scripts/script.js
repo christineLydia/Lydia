b9c86110ed046e5a38033d2ead4c6faa73754404
Lydia <liluxiaolizi@gmail.com>
2019-11-26 00:00:58 -0800

    implement artLover page

--- scripts/script.js
+++ scripts/script.js
@@ -39,3 +39,49 @@ document.addEventListener('DOMContentLoaded', (event) => {
         return false;
     });
 });
+
+var imageSlideIndex = 1;
+
+/*need to wait until elements are added to DOM, then "imageSlides" class elements can be selected out by calling showSlides.*/
+document.addEventListener('DOMContentLoaded', (event) => {
+    showSlides(imageSlideIndex);
+});
+
+function plusSlides(x) {
+    showSlides(imageSlideIndex += x);
+}
+
+function selectCurrentSlide(x) {
+    showSlides(imageSlideIndex = x);
+}
+
+function showSlides(x) {
+    var i;
+    var slides = document.getElementsByClassName("imageSlides");
+
+    // only execute when page is on gardener.html
+    if (slides.length == 0) {
+        return;
+    }
+
+    var dots = document.getElementsByClassName("select-dot");
+
+    if (x > slides.length) {
+        imageSlideIndex = 1;
+    }
+
+    if (x < 1) {
+        imageSlideIndex = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[imageSlideIndex - 1].style.display = "block";
+    dots[imageSlideIndex - 1].className += " active";
+}
